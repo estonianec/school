04b5254797bd7a1917fd32af07e92c35b0d87883
estonec <rusecru@gmail.com>
2022-03-16 14:41:56 +0300

    4.2 Done

--- src/main/java/ru/hogwarts/school/scripts421.sql
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -1,10 +1,14 @@
 alter table student
     add constraint age_constraint check (age > 16);
+
 alter table student
     ADD CONSTRAINT name_unique UNIQUE (name);
+
 alter table student
     alter name set not null;
+
 ALTER TABLE faculty
     ADD CONSTRAINT login_pass_unique UNIQUE (color, name);
+
 ALTER TABLE student
     alter age set DEFAULT 20;
